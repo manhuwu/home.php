5e7cf82d19419dde04538b6922a40aaccd100431
manhuwu <manhne14424@gmail.com>
2024-08-20 23:18:47 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo '<h1 style="text-align: center; color: Green;">Hí anh em</h1>';
+echo '<a href="https://classroom.google.com/c/Njk4MjAwNzExNzg2">GG CLASSROOM</a>';
+echo '<a target="_blank" href="https://www.tiktok.com/@_nlv.nhim_/video/7401372124922891527?is_from_webapp=1&sender_device=pc&web_id=7378480313707906576">CHẢI/a>';
+?>
+
+</body>
+</html>
